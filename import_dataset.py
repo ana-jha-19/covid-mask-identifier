d003d97b8f28d85ecdc503837fda3c245945decc
Joseph Maximilian Barberio <josephbarberio99@gmail.com>
2020-11-07 14:38:00 -0500

    Create import_dataset.py

---
+++ import_dataset.py
@@ -0,0 +1,21 @@
+from google.cloud import automl
+
+# TODO(developer): Uncomment and set the following variables
+# project_id = "YOUR_PROJECT_ID"
+# dataset_id = "YOUR_DATASET_ID"
+# path = "gs://YOUR_BUCKET_ID/path/to/data.csv"
+
+client = automl.AutoMlClient()
+# Get the full path of the dataset.
+dataset_full_id = client.dataset_path(
+    project_id, "us-central1", dataset_id
+)
+# Get the multiple Google Cloud Storage URIs
+input_uris = path.split(",")
+gcs_source = automl.GcsSource(input_uris=input_uris)
+input_config = automl.InputConfig(gcs_source=gcs_source)
+# Import data from the input URI
+response = client.import_data(name=dataset_full_id, input_config=input_config)
+
+print("Processing import...")
+print("Data imported. {}".format(response.result()))
